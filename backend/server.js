3843ddacbe7e8ac1cb0c63fd1b35b834253cc7c9
Gonzalo <h.munozampuero@gmail.com>
2025-04-18 11:03:25 -0400

    Fix CORS manual para Render

--- backend/server.js
+++ backend/server.js
@@ -21,15 +21,22 @@ app.use(cors({
   credentials: true
 }));
 
+const allowedOrigins = ["https://oraculix.cl", "http://localhost:5173"];
+
 app.use((req, res, next) => {
-  const allowedOrigins = ["https://oraculix.cl", "http://localhost:5173"];
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-    res.header("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
   }
-  res.header("Access-Control-Allow-Methods", "GET, POST");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  // Responder OPTIONS manualmente si se necesita
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
   next();
 });
 
